e242fb35031db3fd6d4ba42d2c41a3c30d6a6340
WaritTeerapong <warit.teer@kmutt.ac.th>
2024-05-29 15:37:00 +0700

    Update sendEmail.py

--- Backend/users/views/sendEmail.py
+++ Backend/users/views/sendEmail.py
@@ -1,21 +1,33 @@
-from django.core.mail import BadHeaderError, send_mail
+from django.core.mail import BadHeaderError, EmailMessage
 
 from rest_framework.decorators import api_view 
 from rest_framework.response import Response
 from rest_framework import status
 
+from users.models import User
+import environ
+
+env = environ.Env()
 
 @api_view(['POST'])
 def send_email(req):
-    recipient = req.data("admin_email")
-    subject = req.data("subject")
-    message = req.data("message")
-    from_email = req.data("from_email",)
-    
-    if subject and message and from_email:
-        try:
-            
-            send_mail(subject, message, from_email, [recipient]) # send_mail(subject, message, from_email, recipient_list)
-        except BadHeaderError:
-            return Response("Invalid header found.", status=status.HTTP_400_BAD_REQUEST)
-        return Response("Request send", status=status.HTTP_200_OK)
+    try:
+        # Get the image file from the request
+        image_file = req.FILES['image']
+
+        # Create an email message
+        email = EmailMessage(
+            subject=req.data("subject"),
+            body=req.data("body"),
+            from_email=req.data("from_email"),
+            to=[env('official_email')],
+        )
+
+        # Attach the image file
+        email.attach(image_file.name, image_file.read(), image_file.content_type)
+        # Send the email
+        email.send()
+
+    except Exception as error:
+        return Response(f"Error at sendEmail:{error}", status=status.HTTP_400_BAD_REQUEST)
+    return Response("Email send successfully", status=status.HTTP_200_OK)
